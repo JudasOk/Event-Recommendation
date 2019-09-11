fcbef4a5eb2f897db86c681a0ac38fe07c88e160
JudasOk <414976959@qq.com>
2019-09-10 21:42:14 -0400

    content-based event recommendation

---
+++ src/external/GeoHash.java
@@ -0,0 +1,14 @@
+package external;
+
+public class GeoHash {
+	 private static final String BASE_32 = "0123456789bcdefghjkmnpqrstuvwxyz";
+	 
+//	 private static int divideRangeByValue(double value,double[] range) {
+////		 double mid = middle(range);
+//	 }
+	 
+	 
+	 public static void main(String[] args) {
+		System.out.println(0<<1);
+	}
+}
